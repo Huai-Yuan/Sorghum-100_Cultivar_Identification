9b33e2c87120b8699373acd25b3a3a878ad448b9
Huai-Yuan <wayne500000w@gmail.com>
2022-04-19 23:12:27 +0800

    slightly modify the method of data agumentation trying to obtain higher score

--- packages/Model_EfficientNet.py
+++ packages/Model_EfficientNet.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
+from tensorflow.keras.applications.efficientnet_v2 import EfficientNetV2B0
 tf.keras.mixed_precision.set_global_policy('mixed_float16')
 
 def get_model(input_shape, output_shape):
-    base_model = tf.keras.applications.efficientnet_v2.EfficientNetV2B0(input_shape=input_shape,
-                                                                        include_top=False)
+    base_model = EfficientNetV2B0(input_shape=input_shape, include_top=False)
     inputs = tf.keras.Input(shape=input_shape)
     # Transfer Learning
     x = base_model(inputs)
@@ -14,3 +14,7 @@ def get_model(input_shape, output_shape):
 
     model = tf.keras.Model(inputs, outputs)
     return model
+
+if __name__ == '__main__':
+    model = get_model(input_shape=(256, 256, 3), output_shape=100)
+    print(model.summary())
